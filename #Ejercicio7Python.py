347ed77274ec032c0523f6b253b35c91c708ae7e
MiguelTorresss <116587393+MiguelTorresss@users.noreply.github.com>
2022-11-09 12:47:17 -0500

    Add files via upload

---
+++ #Ejercicio7Python.py
@@ -0,0 +1,16 @@
+#Ejercicio7Python
+valores = [5, 1, 9, 2, 7, 4]
+encontrado = False
+indice = 0
+longitud = len(valores)
+while indice < longitud:
+    valor = valores[indice]
+    if valor == 2:
+        encontrado = True
+        break
+    else:
+        indice += 1
+if encontrado:
+    print(f'El elemento 2 ha sido encontrado en el índice {indice}')
+else:
+    print('El elemento 2 no se encuentra en la lista de valores')
